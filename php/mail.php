f36a6243a3eb3eb3586292e1137717f31e51a642
CraigTheNerd <57265250+CraigTheNerd@users.noreply.github.com>
2022-07-07 11:41:04 +0200

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -147,14 +147,14 @@ if (!empty($name) OR !empty($email) OR !empty($message) OR !empty($captcha)) {
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;                         //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host = 'azna.co.za';                     //Set the SMTP server to send through
-        $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-        $mail->Username = 'htmlcontactform@azna.co.za';                     //SMTP username
-        $mail->Password = 'G99HYPtfaQdHhbcW';                               //SMTP password
+        $mail->Host = 'YOUR_SMTP_HOST';                             //Set the SMTP server to send through
+        $mail->SMTPAuth = true;                                     //Enable SMTP authentication
+        $mail->Username = 'YOUR_SMTP_USERNAME';                     //SMTP username
+        $mail->Password = 'YOUR_SMTP_PASSWORD';                     //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $mail->Port = 587;                                          //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
         //Recipients
         $mail->setFrom('info@azna.co.za', 'Azna Technology');
